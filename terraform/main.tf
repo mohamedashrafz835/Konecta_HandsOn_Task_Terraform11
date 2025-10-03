a1fda8a818436d34bdac1cd5cac7342318b2c1b1
Sherif Amin <160179498+SherifAmino@users.noreply.github.com>
2025-10-04 00:08:35 +0300

    commit 3
    vpc importation

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,145 @@ resource "aws_subnet" "main" {
     Project  = "ONE Journey Dev"
   }
 }
+
+resource "aws_subnet" "main2" {
+  vpc_id                  = "vpc-07a8c9cc4874997d8"
+  cidr_block              = "10.133.56.0/27"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Country  = "Espana"
+    Name     = "10.133.56.0/27-Private1-eu-west-1a"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_subnet" "main3" {
+  vpc_id                  = "vpc-07a8c9cc4874997d8"
+  cidr_block              = "10.133.56.32/27"
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Country  = "Espana"
+    Name     = "10.133.56.32/27-Private2-eu-west-1b"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_subnet" "main4" {
+  vpc_id                  = "vpc-07a8c9cc4874997d8"
+  cidr_block              = "10.133.56.96/27"
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Country  = "Espana"
+    Name     = "10.133.56.96/27-Public2-eu-west-1b"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_route_table" "private2" {
+  vpc_id = "vpc-07a8c9cc4874997d8"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "nat-06e0fae801fb51415"
+  }
+
+  tags = {
+    Country  = "Espana"
+    Name     = "RT-Private2"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_route_table" "private1" {
+  vpc_id = "vpc-07a8c9cc4874997d8"
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "nat-056d26997c2efb49d"
+  }
+
+  tags = {
+    Country  = "Espana"
+    Name     = "RT-Private1"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_route_table" "public1" {
+  vpc_id = "vpc-07a8c9cc4874997d8"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "igw-0520f16d01b04bbce"
+  }
+
+  tags = {
+    Country  = "Espana"
+    Name     = "RT-Public1"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_route_table" "public2" {
+  vpc_id = "vpc-07a8c9cc4874997d8"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "igw-0520f16d01b04bbce"
+  }
+
+  tags = {
+    Country  = "Espana"
+    Name     = "RT-Public2"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_nat_gateway" "gw1" {
+  allocation_id = "eipalloc-0125eeaeeb3ed5378"
+  subnet_id     = "subnet-078739331b8ff8b6f"
+
+  tags = {
+    Country  = "Espana"
+    Name     = "NAT-ONE-Journey-Dev-2"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_nat_gateway" "gw2" {
+  allocation_id = "eipalloc-03c0f299953fa6cde"
+  subnet_id     = "subnet-0da11ba137923c147"
+
+  tags = {
+    Country  = "Espana"
+    Name     = "NAT-ONE-Journey-Dev-1"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "vpc-07a8c9cc4874997d8"
+
+  tags = {
+    Country  = "Espana"
+    Name     = "IGW-ONE-Journey-Dev"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
