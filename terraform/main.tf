d0d6ae51991176b28c8bcc239d07501033dc5a19
Sherif Amin <160179498+SherifAmino@users.noreply.github.com>
2025-10-03 23:21:16 +0300

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,24 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
+
   tags = {
     Name     = "10.133.56.0/25-VPC-ONE-Journey-Dev"
     Country  = "Espana"
+    Platform = "Terraform"
+    Project  = "ONE Journey Dev"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = "vpc-07a8c9cc4874997d8"
+  cidr_block              = "10.133.56.64/27"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Country  = "Espana"
+    Name     = "10.133.56.64/27-Public1-eu-west-1a"
     Platform = "Terraform"
     Project  = "ONE Journey Dev"
   }
