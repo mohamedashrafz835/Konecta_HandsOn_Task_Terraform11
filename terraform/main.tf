c0b5ef5691e6ecd576bcc9cdc5eea2c6d38cefa1
mohamedashrafz835 <mohamed.ashraf65@msa.edu.eg>
2025-10-05 00:55:37 +0300

    first lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -341,3 +341,118 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "mybucket_sse" {
     }
   }
 }
+variable "filename" {
+  description = "Path to the Lambda deployment package (zip). If using S3, leave empty and set s3_bucket/s3_key instead."
+  type        = string
+  default     = "lambda.zip"
+}
+
+# Use the existing role ARN from your state
+locals {
+  lambda_role_arn = "arn:aws:iam::305905981536:role/one-journey-dev-test-talkJsWebhook-eu-west-1-lambdaRole"
+}
+
+resource "aws_lambda_function" "talkjs_webhook" {
+  function_name = "one-journey-dev-test-talkJsWebhook"
+  role          = local.lambda_role_arn
+  handler       = "src/api/v1/channel/talkJs/webhook.handler"
+  runtime       = "nodejs20.x"
+  memory_size   = 1536
+  timeout       = 6
+  architectures = ["x86_64"]
+  package_type  = "Zip"
+
+  # If you have the zip locally (recommended in your case), set filename.
+  # Ensure the file exists at the path specified by var.filename.
+  filename         = var.filename
+  source_code_hash = "XgzF+slHBd21U5AzVPFz3UCpIQTkM1DImYHFjlKLI0M="
+
+  # If you instead want to deploy from S3, comment the two lines above and use:
+  # s3_bucket = "your-bucket"
+  # s3_key    = "path/to/your-lambda.zip"
+
+  layers = [
+    "arn:aws:lambda:eu-west-1:305905981536:layer:ffmpegv1:1"
+  ]
+
+  environment {
+    variables = {
+      ACCOUNT_ID                                  = "305905981536"
+      API_ENDPOINT                                = "https://jy2hacoam9.execute-api.eu-west-1.amazonaws.com/dev-test/"
+      AUDIOS_BUCKET                               = "one-journey-audiorecord-dev-test"
+      CDN_ENDPOINT                                = "https://d2edzwz37badk2.cloudfront.net/"
+      COGNITO_USER_POOL                           = "eu-west-1_dDvBEA3nb"
+      COGNITO_USER_POOL_CLIENT                    = "5nqaq6r999704lta43ncu0m2ls"
+      CORE_GDS_EVENT_ARN                          = "arn:aws:lambda:eu-west-1:305905981536:function:one-journey-dev-test-coreGDSEvent"
+      CORE_S3_EVENT_ARN                           = "arn:aws:lambda:eu-west-1:305905981536:function:one-journey-dev-test-coreS3Event"
+      CORS_ORIGIN                                 = "*"
+      DEBUG_LOG                                   = "/aws/one-journey/one-journey-DebugLogs"
+      DOMAIN_SUFFIX                               = "one-journey"
+      DYNAMODB_TABLE_CHANNEL_MESSAGES             = "one-journey-channel-messages"
+      DYNAMODB_TABLE_COMMON_OBJECTS               = "one-journey-common-objects"
+      DYNAMODB_TABLE_COST                         = "one-journey-cost"
+      DYNAMODB_TABLE_COUNTERS_STORAGE_CHANNEL     = "one-journey-counters-storage-channel"
+      DYNAMODB_TABLE_EXECUTIONS                   = "one-journey-executions"
+      DYNAMODB_TABLE_EXECUTIONS_V5                = "one-journey-executions-v5"
+      DYNAMODB_TABLE_EXECUTIONS_V5_DETAIL         = "one-journey-executions-v5-detail"
+      DYNAMODB_TABLE_EXECUTIONS_V5_GSI_CREATED_AT = "one-journey-executions-v5-gsi-createdAt"
+      DYNAMODB_TABLE_EXECUTIONS_V5_GSI_UPDATED_AT = "one-journey-executions-v5-gsi-updatedAt"
+      DYNAMODB_TABLE_GEN_AI_BATCH                 = "one-journey-genAIBatch"
+      DYNAMODB_TABLE_MUTEX_LOCK                   = "one-journey-mutex-lock"
+      DYNAMODB_TABLE_SESSIONS                     = "one-journey-sessions"
+      DYNAMODB_TABLE_STT_OPERATIONS               = "one-journey-sttOperations"
+      DYNAMODB_TABLE_SUSCRIPTIONS                 = "one-journey-suscriptions"
+      DYNAMODB_TABLE_TEMPORARY                    = "one-journey-temporary-resources"
+      DYNAMODB_TABLE_WEBSOCKETS_MANAGER           = "one-journey-websockets-manager"
+      DYNAMODB_TABLE_WORKFLOWS_BACKUP             = "one-journey-workflows-backup"
+      DYNAMODB_TABLE_WORKFLOW_CONTEXT             = "one-journey-workflow-context"
+      ERROR_LOG                                   = "/aws/one-journey/one-journey-ErrorLogs"
+      EVENTBRIDGE_MESSAGE_BUS                     = "one-journey-messages-bus-dev-test"
+      HOSTING_BUCKET                              = "one-journey-hosting-bucket-dev-test"
+      KENDRA_S3_ROLE_ARN                          = "arn:aws:iam::305905981536:role/OneJourneyKendraS3Role"
+      KEYS_URL                                    = "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_dDvBEA3nb/.well-known/jwks.json"
+      MAIN_CONFIGURATION_ID                       = "@MAIN_CONFIGURATION"
+      MAX_CHATGPT_RETRY_REQUEST                   = "5"
+      ONE_JOURNEY_DATA_BUCKET                     = "one-journey-data-bucket-dev-test"
+      ONE_JOURNEY_FILES_BUCKET                    = "one-journey-files-bucket-dev-test"
+      PROCESS_TRANSCRIPTION_ARN                   = "arn:aws:lambda:eu-west-1:305905981536:function:one-journey-dev-test-processTranscription"
+      REGION                                      = "eu-west-1"
+      SQS_BATCH_QUEUE_URL                         = "https://sqs.eu-west-1.amazonaws.com/305905981536/one-journey-batch-queue-dev-test"
+      STAGE                                       = "dev-test"
+      STORAGE_PROCESS_LAMBDA_ARN                  = "arn:aws:lambda:eu-west-1:305905981536:function:one-journey-dev-test-manageFileStorage"
+      STT_WEBHOOK_ARN                             = "arn:aws:lambda:eu-west-1:305905981536:function:one-journey-dev-test-sttWebhook"
+      TRANSCRIPTIONS_BUCKET                       = "one-journey-transcriptions-dev-test"
+      USER_POOL_CLIENT_ID                         = "5nqaq6r999704lta43ncu0m2ls"
+      USER_POOL_ID                                = "eu-west-1_dDvBEA3nb"
+      VERSION                                     = "v1"
+      WEBSOCKET_API_ENDPOINT                      = "wss://r9kbm99j8b.execute-api.eu-west-1.amazonaws.com/dev-test/"
+    }
+  }
+
+  ephemeral_storage {
+    size = 512
+  }
+
+  logging_config {
+    log_format = "Text"
+    log_group  = "/aws/lambda/one-journey-dev-test-talkJsWebhook"
+  }
+
+  tracing_config {
+    mode = "PassThrough"
+  }
+
+  tags = {
+    STAGE = "dev-test"
+  }
+
+  lifecycle {
+    # keep Terraform from trying to replace the function because of AWS-managed fields
+    ignore_changes = [
+      # AWS-controlled attributes you don't manage here:
+      last_modified,
+      qualified_arn,
+      version
+    ]
+  }
+}
