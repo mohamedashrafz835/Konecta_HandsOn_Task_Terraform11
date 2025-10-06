c60b717a517f6aaaf902a482d1cb63ec2dcacd7b
mohamed ashraf <mohamed.ashraf65@msa.edu.eg>
2025-10-06 13:33:46 +0300

    Create main.tf ecr module

---
+++ modules/ecr/main.tf
@@ -0,0 +1,14 @@
+resource "aws_ecr_repository" "main" {
+  name                 = var.name
+  image_tag_mutability = var.image_tag_mutability
+
+  encryption_configuration {
+    encryption_type = var.encryption_type
+  }
+
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+
+  tags = var.tags
+}
