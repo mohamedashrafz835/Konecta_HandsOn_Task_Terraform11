1d1befcb1ec033399042d42902e60d929873a6f8
mohamed ashraf <mohamed.ashraf65@msa.edu.eg>
2025-10-06 13:53:26 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -62,3 +62,44 @@ variable "ecr_tags" {
   default     = {}
 }
 
+variable "sg_name" {
+  description = "Security Group name"
+  type        = string
+}
+
+variable "sg_description" {
+  description = "Security Group description"
+  type        = string
+}
+
+variable "sg_vpc_id" {
+  description = "VPC ID where Security Group will be created"
+  type        = string
+}
+
+variable "sg_ingress_rules" {
+  description = "Ingress rules for the Security Group"
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "sg_egress_rules" {
+  description = "Egress rules for the Security Group"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "sg_tags" {
+  description = "Tags for the Security Group"
+  type        = map(string)
+}
+
